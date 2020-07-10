376125bdcf93ffc79b0979f26c3e9e0d177a7183
Dominik Stodolny <dominik@stodolny.org>
2020-07-10 17:28:03 +0100

    reuse current dired buffer

--- init.el
+++ init.el
@@ -694,6 +694,10 @@
   :config
   (setq trashed-action-confirmer 'y-or-n-p)
   (setq trashed-date-format "%Y-%m-%d %H:%M:%S"))
+(use-package dired-single
+  :bind (([remap dired-find-file] . dired-single-buffer)
+         ([remap dired-mouse-find-file-other-window] . dired-single-buffer-mouse)
+         ([remap dired-up-directory] . dired-single-up-directory)))
 
 ;; Proced
 (use-package proced
