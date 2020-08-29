d7ad0ce512ef8932a339eb876972dfc1555ce334
Dominik Stodolny <dominik@stodolny.org>
2020-08-29 14:36:27 +0100

    add magit hooks for diff-hl

--- init.el
+++ init.el
@@ -81,7 +81,9 @@
 (autoload 'magit "magit" "Magit" t)
 (when (require 'diff-hl nil t)
   (setq diff-hl-draw-borders nil)
-  (add-hook 'after-init-hook 'global-diff-hl-mode))
+  (add-hook 'after-init-hook 'global-diff-hl-mode)
+  (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
 
 
 ;;; Key bindings
